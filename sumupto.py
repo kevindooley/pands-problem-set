9ec0cf0f31766cdee8cd420f237b009d2baa3e1a
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-02 21:36:36 +0000

    tidied up sumupto.py and 1 attempts at problem 4 and 5

--- sumupto.py
+++ sumupto.py
@@ -1,19 +1,22 @@
 #Kevin Dooley 24th Feb 19
 #Solution to question 1
 #Write a program that asks the user to input any positive integer
-#outputs outputs the sum of all numbers between one and that number
+#outputs the sum of all numbers between one and that number
 
 x = int(input("enter a positive number"))
 
 
 #question asks for a positive number
-#if statement to tell user to enter a number greater than 0
-if x <= 0:
+#used if statement incase negative no. used
+#this tells the user to enter a number greater than 0
+if x < 0:
     print ("Please enter a positive number")
 
-#if else statement - if x > 0 use Gauss formula to caluculate the sum
+#if else statement used if numbered entered above 0
+#researched mathematical formula that calculates sum of any integer
 
 elif x > 0:
+#discover gauss formula to caluclate the sum of
     total = int(x*(x+1)/2)
     print("The sum is", total)
 
