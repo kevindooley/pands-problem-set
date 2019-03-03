324f9250d95ceab8d362e4c91d139375c7f31d95
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-03 13:30:20 +0000

    attempt at problem 4, struggling to develop solution

--- python-collatz.py
+++ python-collatz.py
@@ -12,13 +12,18 @@ x = int(input("please enter a positive number"))
 if x <= 0:
     print ("Please enter a positive number")
 
-for i in range(x):
-    if i % 2 ==0:
-        print (i//2)
+while x >=1:
+    if x % 2 ==0:
+        print(x//2)
+    return x//2
 
-    elif i % 2 !=0:
-        print ((i * 3) + 1)
+    elif x % 2 !=0:
+        print((x * 3) + 1)
+        
 
+
+    
+    
 
         
     
