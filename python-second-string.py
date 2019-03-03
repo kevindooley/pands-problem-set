909cdc3edf124d0fd6e9fba61488b85769296388
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-03 22:45:56 +0000

    creating file for python-second-string.py

---
+++ python-second-string.py
@@ -0,0 +1,9 @@
+#Kevin Dooley 03 Mar 19
+#Write a program that takes an input string
+#Outputs every second word
+
+a = ('the', 'quick', 'brown', 'fox,' 'jumps', 'over', 'the', 'lazy', 'dog')
+
+for i in a:
+    print(i)
+
