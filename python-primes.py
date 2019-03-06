16e32f4ebbab889075814528124782344adfbf71
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-06 21:19:33 +0000

    Corrected code to say to ask for a positive integer if one is not entered

--- python-primes.py
+++ python-primes.py
@@ -9,31 +9,38 @@
 #because anything less than 2 isnt a prime number
 i = int(input("Enter a positive integer"))
 
-if i <= 1:
-    print ("Enter a positive integer above one as one is neither a prime or composite integer")
     
 #for statement used so there is a looping sequence
 #until the indented statements are false
 #range function only starts at 2 because 2 is the 1st prime number
 #i is the inputed integer#
 #i in the range function is upto but not including i
-    for n in range(2, i):
-
+for n in range(2, i):
+    if i < 0:
+        break
 #if statement within for loop
 #a prime number can only be divided by 1 & itself
 #i % n ==0 means any number that gives no remainder it divides into i
 #it cannot be a prime number
-        if i % n ==0:
-            print(i, "is not a prime number")
+    if i % n ==0:
+        print(i, "is not a prime number")
 #break statement used within for loop
 #the break statement means the for loop is broken when 
 #the if statement above it is no longer true
-            break
+        break
 
 #because the break statement has broken the loop
 #the else statement now kicks in outside of the for loop
 else:
+    if i > 0:
 #i did not meet the criteria set in the for loop
-    print(i, "is a prime number")
+        print(i, "is a prime number")
+
+    else: 
+        if i < 0:
+            print("A positive integer is required, Try again")
+
+
+
 
 
