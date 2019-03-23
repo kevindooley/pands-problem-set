f74b281f44e960cb8e28fcc86c8209027116cc3d
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-23 19:57:54 +0000

    solution & comments for python-second-string.py)

--- python-second-string.py
+++ python-second-string.py
@@ -2,11 +2,23 @@
 #Write a program that takes an input string
 #Outputs every second word
 
+
+#ask the user to enter a word on the command line#
+#stored as variable 'sentence'
 sentence = input("Please type any sentence")
 
-
-
-new_sentence = ' '.join(sentence.split()[::2])
-print (new_sentence)
+#used built in join & split functions
+#created variable 'x' by spliting the initial string input from the user
+#[start:stop:step] - we do not know what the user will enter so we have no start or end point
+#we want every sencond word so use step of 2
+#split function has turned variable 'sentence' into a list containing every second word
+x = sentence.split()[::2]
+print (x)
+#created 3rd variable 'second_word' 
+#used join function to turn list 'x' back to a string
+#enter a string first - in this case a space (' ') because I want  a space between the words
+#variable 'second_word' is now a string
+second_word = ' '.join(x)
+print(second_word)
 
 
