7298feda17bef8bd2e9a91701f8cf8a231b5ebef
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-27 22:24:38 +0000

    Minor changes to the comments for todaysbeginswithT.py (solution 2)

--- todaybeginswithT.py
+++ todaybeginswithT.py
@@ -1,9 +1,10 @@
 #Kevin Dooley 24th Feb 19
+#Solution question 2
 #A Program that outputs what day it is
 #whether today is a day that starts with a T
 
 
-#import allows you to get basic date and time from computer
+#Import is a bult in function that allows you to get basic date and time information from the computer
 import datetime
 
 #if statement used to see if condition is true
@@ -15,12 +16,12 @@ if datetime.datetime.today().weekday() == 1:
     print("Yes, Today starts with a T")
 
 
-#same rationale as comments 9 to 12
+#same rationale as comments 10 to 13
 if datetime.datetime.today().weekday() == 3:
     print("Yes, Today starts with a T")
 
-#when the if statement doesnt apply
-#because the above if statements are not equal to 1 or 3
+#when the if statements dont apply
+#because the above 'if' statements are not equal to 1 or 3
 #else statement used to print saying today not day starting with T
 else:
     print("No, today does not start with a T")
