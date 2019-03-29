a7f5630e6876a967736f0a169e42a9c203d1edef
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-29 22:56:14 +0000

    Made solution more concise and added comments

--- python-divisors.py
+++ python-divisors.py
@@ -3,13 +3,13 @@
 #that are divisable by 6 but not by 12
 
 
-#i is the number at any given time with the for loop
-#the for loop iterates through the algorithim in the given order
-#using the range of 1000 to 10000 and then stops
+#i is the number at any given time with the 'for' loop
+#the 'for' loop iterates through the algorithim in the given range
+#using the range of 1000 to 10000 and then stops [Start:Stop:Step]
 #range funtion used to set numbering start and end point
 
 #this range starts at 1000, stops at but does not include 10000
-#stepping/incrementing in 1
+#stepping/incrementing in 1 - no step mentioned, automatically 1
 for i in range( 1000, 10000):
 
 #indented as part of for loop
@@ -17,13 +17,11 @@ for i in range( 1000, 10000):
 #arithmetic operator %(modulo of a number) means dividing it by that number 
 #and have a remainder of 0 ie gives a whole number
 #comparision operator == means equal to 
-    if i % 6 ==0:
-#the number cannot be divisable by 12
-#same as line 12 except comparision operator !=0
-#means not equal to zero
-#if i is divided by 12 and has a remainder it is to be excluded from the answer
 
-        if i % 12 !=0:
-            print(i)
+    if i % 6 ==0 and i % 12 !=0:
+#the number cannot be divisable by 12
+#same as line 21 except comparision operator !=0 (not equal to 0)
+#if i is divided by 12 and has a remainder it is to be excluded from the answer
+        print(i)
 
 
