2ba9a9a216d8f08df1ad213e8d898944c2d0ad29
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-30 01:47:26 +0000

    removed part regarding if a whole number was inputted by the user

--- python-squareroot.py
+++ python-squareroot.py
@@ -8,27 +8,18 @@
 
 x = float(input("Please enter a positive floating number: "))
 
-if x < 0:
-    print("please enter a positive floating number")
 
 #Used the built in math function
 #allows use to different mathematical functions#
 #in this case we want to find the square root math.sqrt
 # shorten sqrt to sq - math import sqrt as sq
-
 from math import sqrt
-#while loop will run as long as x is a floating point number
-#otherwise will break and go to print statment at the bottom 
-while x != float:
-    print("enter a decimal number")
-    break
-    
-if  
+
 #ans is square root in decimal form
-    ans = sqrt(x)
+ans = sqrt(x)
 #ans1 becomes approx square root using round
-    ans1 = round(ans, 1)
-    print("The square root of", x, "is approx", ans1)
+ans1 = round(ans, 1)
+print("The square root of", x, "is approx", ans1)
 
 
     
