e1da33142089b2c584a85cc495dd5654f16a481b
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-30 10:47:14 +0000

    Added code so program can run as argument off command line & added comments

--- python-second.py
+++ python-second.py
@@ -7,28 +7,32 @@
 #by using 'with' and indenting my code
 #there is no need to use 'close()' to close the file
 #the file automatically closes once the loop has finished.
-with open('myfile.txt', 'r') as f:
+
+#import sys - this nuilt in module allows you to take argurments from the command line
+import sys
+
+#program will run if there are 2 filenames on the command line
+#otherwise else conditional will ask user to try again
+if len(sys.argv) == 2:
+
+    with open(sys.argv[1], 'r') as f:
+
 
 #created variable 'x' to be used in for loop
 #'x' will be iterated through the loop until there are no more lines left in the text file
-    x = 0
+        x = 0
 
-    for line in f:
+        for line in f:
 #'x' iterated by 1 until no more lines in the text file
-        x+=1
+            x+=1
 
 #'if' statement in the for loop because indented
 #'if' statement says - if x divided by 2 does not give a remainder 
 #print that line
 #I have set the program to print the odd lines of the text.
-        if x % 2 != 0:
-            print(line)
+            if x % 2 != 0:
+                print(line)
 
-        
+else:
+    print("Please provide two filenames to run this program")
 
-        
-
-
-
-
-   
