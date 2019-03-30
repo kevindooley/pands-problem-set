4c84ed69ce996caf1601703dd0ba9a9d434f697d
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-30 02:22:31 +0000

    Changed solution to tell user to try again if negative/whole number entered. Comments added (lines 17 - 26)

--- python-squareroot.py
+++ python-squareroot.py
@@ -8,18 +8,27 @@
 
 x = float(input("Please enter a positive floating number: "))
 
-
 #Used the built in math function
 #allows use to different mathematical functions#
 #in this case we want to find the square root math.sqrt
 # shorten sqrt to sq - math import sqrt as sq
 from math import sqrt
 
-#ans is square root in decimal form
-ans = sqrt(x)
-#ans1 becomes approx square root using round
-ans1 = round(ans, 1)
-print("The square root of", x, "is approx", ans1)
+#asks user to try again if negative number entered
+if x <= 0:
+    print("Please enter a positive decimal number")
+
+#asks user to try again if a floating number is not entered
+if x > 0 and x == int(x):
+        print("Please try again and enter a decimal number")
+        
+#runs below code is number entered is a decimal number
+elif x > 0 and x == float(x):
+#ans = the square root of x in decimal form
+        ans = sqrt(x)
+#ans_1 becomes approx square root (to 1 decimal place) using round function
+        ans_1 = round(ans, 1)
+        print("The square root of", x, "is approx", ans_1)
 
 
     
