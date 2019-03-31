2868ef94a5f376aa064b3886f4cd52ca57ddedd8
Kevin Dooley <kevindooley11@hotmail.com>
2019-03-31 00:11:43 +0000

    fixed minor bug - lines 13 & 15 slightly changed

--- secondstring.py
+++ secondstring.py
@@ -10,10 +10,10 @@ sentence = input("Please type any sentence: ")
 #used built in join & split functions
 #created variable 'x' by spliting the initial string input from the user
 #[start:stop:step] - we do not know what the user will enter so we have no start or end point
-#we want every second word so use step of 2
+#split at every space and we want every second word so use slice of 2
 #split function has turned variable 'sentence' into a list containing every second word
-x = sentence.split()[::2]
-print (x)
+x = sentence.split(' ')[::2]
+
 #created 3rd variable 'second_word' 
 #used join function to turn list 'x' back to a string
 #enter a string first - in this case a space (' ') because I want  a space between the words
